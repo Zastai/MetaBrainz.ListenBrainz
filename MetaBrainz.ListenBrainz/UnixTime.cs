e4ad5c6a69f556ed332cd01a818ef520c5d604d5
Tim Van Holder <tim.vanholder@gmail.com>
2020-03-27 20:49:55 +0100

    Added overloads Convert() taking/returning nullables.
    Also, use DateTime.UnixEpoch when available.

--- MetaBrainz.ListenBrainz/UnixTime.cs
+++ MetaBrainz.ListenBrainz/UnixTime.cs
@@ -10,18 +10,32 @@ namespace MetaBrainz.ListenBrainz {
   [PublicAPI]
   public static class UnixTime {
 
-    /// <summary>The epoch for Unix time values (1970-01-01T00:00:00).</summary>
+    /// <summary>The epoch for Unix time values (1970-01-01T00:00:00Z).</summary>
+#if NETSTD_GE_2_1 || NETCORE_GE_2_1
+    public static readonly DateTime Epoch = DateTime.UnixEpoch;
+#else
     public static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+#endif
 
     /// <summary>Computes the Unix time value corresponding to the specified date/time.</summary>
     /// <param name="value">The date/time to convert to a Unix time value.</param>
     /// <returns>The corresponding Unix time value.</returns>
     public static long Convert(DateTime value) => (long) (value - UnixTime.Epoch).TotalSeconds;
 
+    /// <summary>Computes the Unix time value corresponding to the specified date/time.</summary>
+    /// <param name="value">The date/time to convert to a Unix time value.</param>
+    /// <returns>The corresponding Unix time value.</returns>
+    public static long? Convert(DateTime? value) => value.HasValue ? (long?) (value.Value - UnixTime.Epoch).TotalSeconds : null;
+
     /// <summary>Computes the date/time corresponding to the specified Unix time value.</summary>
     /// <param name="value">The Unix time value to convert to a date/time.</param>
     /// <returns>The corresponding date/time.</returns>
     public static DateTime Convert(long value) => UnixTime.Epoch.AddSeconds(value);
+
+    /// <summary>Computes the date/time corresponding to the specified Unix time value.</summary>
+    /// <param name="value">The Unix time value to convert to a date/time.</param>
+    /// <returns>The corresponding date/time.</returns>
+    public static DateTime? Convert(long? value) => value.HasValue ? (DateTime?) UnixTime.Epoch.AddSeconds(value.Value) : null;
 
     /// <summary>A JSON converter for serializing Unix time values to/from .NET <see cref="DateTime"/> values.
     /// </summary>
