1bb1e5d70ec2a17bb3f8a8883710904673f6f547
Tim Van Holder <tim.vanholder@gmail.com>
2020-03-30 23:13:40 +0200

    Extended doc comments.

--- MetaBrainz.ListenBrainz/Interfaces/IFetchedListens.cs
+++ MetaBrainz.ListenBrainz/Interfaces/IFetchedListens.cs
@@ -8,6 +8,17 @@ using MetaBrainz.Common.Json;
 namespace MetaBrainz.ListenBrainz.Interfaces {
 
   /// <summary>Information about a set of fetched listens.</summary>
+  /// <remarks>
+  /// Which fields are set, aside from <see cref="Count"/> and <see cref="Listens"/>, which should always be present, depends on
+  /// the kind of listen request.<br/>
+  /// In the case of <see cref="ListenBrainz.GetPlayingNow(string)"/>, <see cref="PlayingNow"/> will be set to
+  /// <see langword="true"/> and <see cref="User"/> will be set to the specified user name.<br/>
+  /// When <see cref="ListenBrainz.GetRecentListens(string[])"/> is used, only <see cref="UserList"/> will be set, to the
+  /// specified list of user, separated by commas.<br/>
+  /// In the most common case, using <see cref="ListenBrainz.GetListens(string,int?)"/>, <see cref="User"/> will be set to the
+  /// specified user name, and <see cref="Timestamp"/> will be set to the timestamp of the most recent listen included in
+  /// <see cref="Listens"/> (or <see cref="UnixTime.Epoch"/> if no listens were returned).
+  /// </remarks>
   [PublicAPI]
   public interface IFetchedListens : IJsonBasedObject {
 
@@ -20,7 +31,7 @@ namespace MetaBrainz.ListenBrainz.Interfaces {
     /// <summary>Indicates whether this set of listens represents a currently playing track.</summary>
     bool? PlayingNow { get; }
 
-    /// <summary>The timestamp of the newest listen for the user specified by <see cref="User"/>.</summary>
+    /// <summary>The timestamp of the newest listen included in <see cref="Listens"/>.</summary>
     DateTime? Timestamp { get; }
 
     /// <summary>The MusicBrainz ID of the user for which the listens were fetched.</summary>
