1fd86b1451fe5aa1ec67b5ac652e00738bb3a1dc
Tim Van Holder <tim.vanholder@gmail.com>
2021-11-14 19:58:20 +0100

    Add more items of additional info
    Taken from the documentation:
    - `MediaPlayer` and `MediaPlayerVersion`
    - `MusicService` and `MusicServiceName`
    - `OriginUrl`
    - `SubmissionClient` and `SubmissionClientVersion`
    
    This also fixes the `SpotifyId` field, which was being given the same
    value as `SpotifyAlbumId`.

--- MetaBrainz.ListenBrainz/Interfaces/IAdditionalInfo.cs
+++ MetaBrainz.ListenBrainz/Interfaces/IAdditionalInfo.cs
@@ -47,6 +47,12 @@ public interface IAdditionalInfo {
   /// <summary>The application used to listed to the track.</summary>
   string? ListeningFrom { get; }
 
+  /// <summary>The name of the program used to listen to the track; this should not include a version number.</summary>
+  string? MediaPlayer { get; }
+
+  /// <summary>The version of the program used to listen to the track.</summary>
+  string? MediaPlayerVersion { get; }
+
   /// <summary>The MessyBrainz ID for the track's artist.</summary>
   Guid? MessyArtistId { get; }
 
@@ -58,6 +64,26 @@ public interface IAdditionalInfo {
 
   /// <summary>The MusicBrainz ID for the track's recording.</summary>
   Guid? RecordingId { get; }
+
+  /// <summary>
+  /// If the track comes from an online service, the canonical domain of this service (so "spotify.com", not something like
+  /// "http://open.spotify.com").
+  /// </summary>
+  string? MusicService { get; }
+
+  /// <summary>
+  /// If the track comes from an online service, a name that represents the service. Only relevant when <see cref="MusicService"/>
+  /// is not set.
+  /// </summary>
+  string? MusicServiceName { get; }
+
+  /// <summary>
+  /// If the song of this listen comes from an online source, the URL to the place where it was available at the time.<br/>
+  /// This could be a spotify url (see <see cref="SpotifyId"/>), a YouTube video URL, a Soundcloud recording page URL, or the full
+  /// URL to a public MP3 file. If there is a webpage for this song (e.g. YouTube page, Soundcloud page), that should be used here
+  /// instead of the URL to an actual audio resource.
+  /// </summary>
+  Uri? OriginUrl { get; }
 
   /// <summary>The artist name for the track's release.</summary>
   string? ReleaseArtistName { get; }
@@ -82,6 +108,15 @@ public interface IAdditionalInfo {
 
   /// <summary>The track's Spotify ID.</summary>
   Uri? SpotifyId { get; }
+
+  /// <summary>
+  /// The name of the client used to submit the listen to ListenBrainz; this should not include a version number.<br/>
+  /// If the media player has the ability to submit listens built in, then this value may be the same as <see cref="MediaPlayer"/>.
+  /// </summary>
+  string? SubmissionClient { get; }
+
+  /// <summary>The version of the client used to submit the listen to ListenBrainz.</summary>
+  string? SubmissionClientVersion { get; }
 
   /// <summary>The track's tags.</summary>
   IReadOnlyList<string?>? Tags { get; }
