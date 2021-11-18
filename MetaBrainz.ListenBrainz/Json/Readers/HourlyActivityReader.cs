b984b5db1200b9596233bc33512dbdd0458501d6
Tim Van Holder <tim.vanholder@gmail.com>
2021-11-18 02:52:34 +0100

    Use pattern matching

--- MetaBrainz.ListenBrainz/Json/Readers/HourlyActivityReader.cs
+++ MetaBrainz.ListenBrainz/Json/Readers/HourlyActivityReader.cs
@@ -41,7 +41,7 @@ internal sealed class HourlyActivityReader : ObjectReader<HourlyActivity> {
     if (!hour.HasValue) {
       throw new JsonException("Expected hour not found or null.");
     }
-    if (hour < 0 || hour > 23) {
+    if (hour is < 0 or > 23) {
       throw new JsonException($"The specified hour ({hour}) is out of range (should be 0-23).");
     }
     if (!listenCount.HasValue) {
